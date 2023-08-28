3d566d45a6b4f078861d619173749c87b66f026b
mkoroleva5 <mkoroleva5@gmail.com>
2023-08-28 19:50:23 +0300

    feat: create field sizes view

---
+++ src/views/FieldSizesView/FieldSizesView.ts
@@ -0,0 +1,58 @@
+import { state, stateObserver } from 'store/store';
+import { SizeType } from 'types/SizeType';
+import { createElement } from 'utils/createElement';
+
+export const createFieldSizesView = (parent: HTMLElement) => {
+  const fieldSizes = createElement('div', 'field-size-wrapper');
+
+  const sizes = [3, 4, 5, 6, 7, 8];
+  const sizesArray: SizeType[] = [];
+
+  sizes.map((size) => {
+    const fieldSizeInput = createElement<HTMLInputElement>('input', 'field-size-input');
+
+    fieldSizeInput.id = `input-${size}`;
+    fieldSizeInput.type = 'radio';
+    fieldSizeInput.name = 'field-size';
+
+    const fieldSizeLabel = createElement<HTMLLabelElement>('label', 'field-size-label');
+
+    fieldSizeLabel.htmlFor = `input-${size}`;
+    fieldSizeLabel.textContent = `${size}x${size}`;
+
+    fieldSizes.appendChild(fieldSizeInput);
+    fieldSizes.appendChild(fieldSizeLabel);
+
+    sizesArray.push({
+      size: size,
+      input: fieldSizeInput,
+      label: fieldSizeLabel,
+    });
+  });
+
+  const updateFieldSizes = (sizesArray: SizeType[], gridSize: number) => {
+    sizesArray.map((sizeData) => {
+      const { size, input, label } = sizeData;
+
+      if (gridSize === size) {
+        input.checked = true;
+        label.style.border = '3px solid #829bd6';
+        label.style.color = '#8ce068';
+      } else {
+        input.checked = false;
+        label.style.border = 'none';
+        label.style.color = '#829bd6';
+      }
+    });
+  };
+
+  updateFieldSizes(sizesArray, state.gridSize);
+
+  stateObserver.subscribe(['gridSize'], (newState) => {
+    updateFieldSizes(sizesArray, newState.gridSize);
+  });
+
+  parent.appendChild(fieldSizes);
+
+  return { sizesArray };
+};
