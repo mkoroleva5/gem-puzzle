143d219755eaf4c54c26690719d1f12b8610d19e
mkoroleva5 <mkoroleva5@gmail.com>
2023-08-28 22:50:27 +0300

    fix: adjust counting inversions function

--- src/controllers/GameFieldController/createCellsArray.ts
+++ src/controllers/GameFieldController/createCellsArray.ts
@@ -1,9 +1,9 @@
-const createCellsValuesArray = (cellsArrayLength: number) => {
+/* const createCellsValuesArray = (cellsArrayLength: number) => {
   return Array.from(
     {
       length: cellsArrayLength,
     },
-    (_, index) => index,
+    (_, index) => index + 1,
   ).sort(() => Math.random() - 0.5);
 };
 
@@ -22,17 +22,54 @@ const countInversions = (valuesArray: number[], gridSize: number) => {
 };
 
 export const createCellsValues = (gridSize: number) => {
-  const cellsArrayLength = gridSize * gridSize;
+  const cellsArrayLength = gridSize * gridSize - 1;
   let cellsArray = createCellsValuesArray(cellsArrayLength);
 
-  if (!countInversions(cellsArray, gridSize)) {
-    countInversions(cellsArray, gridSize);
+  if (countInversions(cellsArray, gridSize) % 2 !== 0) {
+    do {
+      cellsArray = createCellsValuesArray(cellsArrayLength);
+    } while (countInversions(cellsArray, gridSize) % 2 !== 0);
   }
 
-  while (countInversions(cellsArray, gridSize) % 2 !== 0) {
-    cellsArray = createCellsValuesArray(cellsArrayLength);
-    countInversions(cellsArray, gridSize);
+  return cellsArray;
+};
+ */
+
+const shuffleArray = (array: number[]) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+
+    [array[i], array[j]] = [array[j], array[i]];
   }
+};
+
+const isSolvable = (array: number[]) => {
+  let inversions = 0;
+  const length = array.length;
+  const emptyRow = Math.floor(array.indexOf(0) / Math.sqrt(length));
+
+  for (let i = 0; i < length; i++) {
+    for (let j = i + 1; j < length; j++) {
+      if (array[i] > array[j] && array[i] !== 0 && array[j] !== 0) {
+        inversions++;
+      }
+    }
+  }
+
+  if (length % 2 === 0) {
+    return (inversions + emptyRow) % 2 === 0;
+  } else {
+    return inversions % 2 === 0;
+  }
+};
+
+export const createCellsValues = (gridSize: number) => {
+  const cellsArrayLength = gridSize * gridSize;
+  let cellsArray = Array.from({ length: cellsArrayLength }, (_, index) => index);
+
+  do {
+    shuffleArray(cellsArray);
+  } while (!isSolvable(cellsArray));
 
   return cellsArray;
 };
