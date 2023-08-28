0273dd79e634863c0e071b65a20dda9c095447a9
mkoroleva5 <mkoroleva5@gmail.com>
2023-08-28 19:52:14 +0300

    feat: update index.ts with game initialization

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,49 @@ import { createGameModel } from 'models/GameModel';
 import { createGameFieldView } from 'views/GameFieldView/GameFieldView';
 import { createButtonsView } from 'views/ButtonViews/ButtonsView';
 import { createResultsTableView } from 'views/ResultsTableView';
+import { createGameStateView } from 'views/GameStateView/GameStateView';
+import { createStartButtonController } from 'controllers/ButtonControllers/StartButtonController';
+import { state } from 'store/store';
+import { createStopButtonController } from 'controllers/ButtonControllers/StopButtonController';
+import {
+  createGameFieldController,
+  updateCellsArray,
+} from 'controllers/GameFieldController/GameFieldController';
+import { createFieldSizesView } from 'views/FieldSizesView/FieldSizesView';
+import { createFieldSizesController } from 'controllers/FieldSizesController/FieldSizesController';
+import { createSaveButtonController } from 'controllers/ButtonControllers/SaveButtonController';
+import { createLoadButtonController } from 'controllers/ButtonControllers/LoadButtonController';
 
 const root = document.getElementById('root');
+
+updateCellsArray();
+const gridSize = state.gridSize;
+const cellsArray = state.cellsArray;
+const results = state.results;
 const gameModel = createGameModel();
-const gridSize = gameModel.getState().gridSize;
-const results = gameModel.getState().results;
 
 const buttonsView = createButtonsView(root);
-const gameField = createGameFieldView(root, gridSize);
+
 const resultsTableView = createResultsTableView(root, results);
-gameField.updateField();
+const gameStateView = createGameStateView(root);
+const gameFieldView = createGameFieldView(root);
+const fieldSizesView = createFieldSizesView(root);
+
+gameModel.createField();
+
+createFieldSizesController(fieldSizesView.sizesArray);
+
+createStartButtonController(buttonsView.startButton, () => {
+  gameModel.createField();
+  gameModel.startGame();
+});
+
+createStopButtonController(
+  buttonsView.stopButton,
+  gameModel.stopGame,
+  gameModel.resumeGame,
+);
+
+createSaveButtonController(buttonsView.saveButton);
+createLoadButtonController(buttonsView.loadButton, gameModel.loadGame);
+createGameFieldController(gameModel.moveCell);
