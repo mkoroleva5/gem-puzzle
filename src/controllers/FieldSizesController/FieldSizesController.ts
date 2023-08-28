1d7804f1ed66421a4334888cedd1163d83e909c9
mkoroleva5 <mkoroleva5@gmail.com>
2023-08-28 22:23:49 +0300

    feat: add game start on field size change

--- src/controllers/FieldSizesController/FieldSizesController.ts
+++ src/controllers/FieldSizesController/FieldSizesController.ts
@@ -1,14 +1,17 @@
 import { state, stateObserver } from 'store/store';
 import { SizeType } from 'types/SizeType';
 
-export const createFieldSizesController = (sizesArray: SizeType[]) => {
+export const createFieldSizesController = (
+  sizesArray: SizeType[],
+  startGame: () => void,
+) => {
   sizesArray.map((sizeData) => {
     const { size, input } = sizeData;
 
     input.addEventListener('click', () => {
       state.gridSize = size;
       stateObserver.broadcast('gridSize', state.gridSize);
-      console.log(state);
+      startGame();
     });
   });
 };
